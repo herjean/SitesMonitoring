54bf1e614935226b811b2ac0d71171363e5a99af
herjean <wong0786@e.ntu.edu.sg>
2016-07-12 00:29:55 +0800

    Add project files.

---
+++ WebsiteMonitoring/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WebsiteMonitoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SiteMonitor.PingUrls(Settings1.Default.SitesToMonitor);
+        }
+    }
+}
---
+++ WebsiteMonitoring/SiteMonitor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net;
+
+namespace WebsiteMonitoring
+{
+    internal static class SiteMonitor
+    {
+        internal static void PingUrls(StringCollection urlsToPing)
+        {
+            foreach (string url in urlsToPing)
+            {
+                try
+                {
+                    var webRequest = HttpWebRequest.Create(url) as HttpWebRequest;
+                    webRequest.Method = WebRequestMethods.Http.Get;
+
+
+                    using (var webResponse = webRequest.GetResponse() as HttpWebResponse)
+                    {
+                        if (webResponse.StatusCode != HttpStatusCode.OK)
+                        {
+                            throw new WebException(string.Format("The response returned error code {0}: {1}", webResponse.StatusCode, webResponse.StatusDescription));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Site: " + url + " is Up");
+                        }
+                    }
+                }
+                catch (WebException webException)
+                {
+                    LogWebException(url, webException);
+                }
+            }
+        }
+
+        private static void LogWebException(string url, WebException webException)
+        {
+            //TODO: Log, Notify, etc. 
+        }
+
+    }
+}
